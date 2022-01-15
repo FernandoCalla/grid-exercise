b68d65dcd8d05ce508fa573679282731f7589d00
FernandoCalla <fernandorafaelcallayarihuaman@gmail.com>
2022-01-15 00:02:19 -0500

    Se realizo el reto

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Cuadricula from './Cuadriculas'
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Cuadricula/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
